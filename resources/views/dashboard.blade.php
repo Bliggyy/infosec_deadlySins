e43097f4a3783b3a401bcbbd4a8c425e25084d18
Bliggyy <blighstian23@gmail.com>
2021-10-02 21:33:52 +0800

    added some vtuber shit for my own self pleasure

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,12 +4,16 @@
             {{ __('Dashboard') }}
         </h2>
     </x-slot>
+    
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
                     You're logged in!
+                </div>
+                <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
+                    <img src="{{ asset('/images/suisei.gif') }}" class="img-fluid ml-auto mr-auto w-auto text-gray-700" alt="Nakiri Ayame">
                 </div>
             </div>
         </div>
